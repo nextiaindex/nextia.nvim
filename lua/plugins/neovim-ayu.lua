6c1916ddd5644bea5ed411a6752eba750ba01a2c
NextiaIndex <nextiaindex@protonmail.com>
2025-09-14 17:03:43 +0700

    Move codes from ayumirage.lua to neovim-ayu.lua.

--- lua/plugins/neovim-ayu.lua
+++ lua/plugins/neovim-ayu.lua
@@ -1,4 +1,14 @@
 return {
 	"Shatur/neovim-ayu",
-	lazy = false
+	lazy = false,
+	config = function ()
+		require("ayu").setup({
+			mirage = true,
+			overrides = {
+				LineNr = { fg = "gray" },
+				CursorLineNr = { fg = "#f28779" },
+			}
+		})
+		vim.cmd("colorscheme ayu-mirage")
+	end
 }
