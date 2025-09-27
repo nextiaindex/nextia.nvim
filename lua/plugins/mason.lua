613a5a754e4a9719cb310c2388af374c5e56ddfb
NextiaIndex <nextiaindex@protonmail.com>
2025-09-27 08:36:28 +0700

    Drop mason-lspconfig implementation
    mason-lspconfig still takes a lot of startup times after lazy loading
    it.

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,23 +1,5 @@
 return {
-	event = "VeryLazy",
-	"mason-org/mason-lspconfig.nvim",
-	dependencies = {
-		{
-			"mason-org/mason.nvim",
-			opts = {},
-			cmd = { "Mason", "MasonInstall", "MasonUpdate" }
-		},
-		"neovim/nvim-lspconfig"
-	},
-	opts = {
-		ensure_installed = {
-			"lua_ls",
-			"emmet_language_server",
-			"cssls",
-			"pyright",
-			"intelephense",
-			"bashls",
-			"ts_ls"
-		}
-	},
+	"mason-org/mason.nvim",
+	opts = {},
+	cmd = { "Mason", "MasonInstall", "MasonUpdate" }
 }
