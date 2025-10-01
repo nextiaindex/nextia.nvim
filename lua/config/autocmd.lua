9e2b0d3a919917ceda56a4fa61d72e470888d0ba
NextiaIndex <nextiaindex@protonmail.com>
2025-10-01 15:06:57 +0700

    Add indenting autocmd for any php files
    After configuring emmet LSP for php usecase, the auto indent doesn't
    work. This solution will fix the issue
    
    Shout out to barraIhsan for the solution.

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -6,3 +6,12 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 		end
 	end
 })
+
+vim.api.nvim_create_autocmd('FIleType', {
+	pattern = 'php',
+	callback = function ()
+		vim.opt_local.indentexpr = ""
+		vim.opt_local.autoindent = true
+		vim.opt_local.smartindent = true
+	end
+})
