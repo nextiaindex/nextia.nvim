4bc5ab7ab192eedea821462e131ed651a9dbb7ea
NextiaIndex <nextiaindex@protonmail.com>
2025-10-01 15:05:22 +0700

    Configure emmet LSP to also work in php files
    I need this for my PHP school project.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,4 +1,18 @@
 vim.lsp.enable('lua_ls')
+vim.lsp.config("emmet_language_server", {
+	filetypes = {
+		'html',
+		'htmldjango',
+		'php'
+	},
+	settings = {
+		emmet = {
+			includeLanguages = {
+				php = 'html'
+			}
+		}
+	}
+})
 vim.lsp.enable('emmet_language_server')
 vim.lsp.enable('cssls')
 vim.lsp.enable('ts_ls')
