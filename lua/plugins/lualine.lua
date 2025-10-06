f51e48b8e116ae87a887c00288158064897d32d8
NextiaIndex <nextiaindex@protonmail.com>
2025-10-06 07:16:22 +0700

    Add lualine plugin
    They look beautiful :).

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return {
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	opts = {
+		options = {
+			component_separators = "",
+			section_separators = ""
+		},
+		sections = {
+			lualine_x = { 'filetype' }
+		}
+	}
+}
