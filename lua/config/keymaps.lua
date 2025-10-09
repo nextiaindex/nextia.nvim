257553d00e31133714caa38a7da4a64587af7e5b
NextiaIndex <nextiaindex@protonmail.com>
2025-10-09 14:42:33 +0700

    Add lazy.nvim and mason.nvim keymaps + comments
    I have achieved a laziness to the point where typing :Lazy and :Mason
    are very tiring.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,10 @@
+-- Open netrw
 vim.keymap.set('n', '<leader>e', ':Ex<CR>')
-
-vim.keymap.set('n', '<leader>-', ':split<CR>')
-vim.keymap.set('n', '<leader>|', ':vsplit<CR>')
+-- Split windows
+	vim.keymap.set('n', '<leader>-', ':split<CR>')
+	vim.keymap.set('n', '<leader>|', ':vsplit<CR>')
+-- 
+-- Open lazy.nvim dashboard
+vim.keymap.set('n', '<leader>l', ':Lazy<CR>')
+-- Open mason.nvim dashbaord
+vim.keymap.set('n', '<leader>l', ':Mason<CR>')
