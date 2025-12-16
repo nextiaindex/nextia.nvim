6ec5379296a8fa9d817fffc2e55d73f165090581
NextiaIndex <nextiaindex@protonmail.com>
2025-12-16 20:04:52 +0700

    Add different indent space for different filetypes
    Sometimes, 8 (Neovim's default indent space) is too long on some file
    types such as html and lua.

---
+++ lua/config/autocmd.lua
@@ -0,0 +1,19 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+augroup('setIndent2', {clear = true})
+autocmd('Filetype', {
+	group = 'setIndent2',
+	pattern = 'css',
+	command = 'setlocal shiftwidth=2 tabstop=2'
+})
+
+augroup('setIndent4', {clear = true})
+autocmd('Filetype', {
+	group = 'setIndent4',
+	pattern = {
+		'html',
+		'lua'
+	},
+	command = 'setlocal shiftwidth=4 tabstop=4'
+})
